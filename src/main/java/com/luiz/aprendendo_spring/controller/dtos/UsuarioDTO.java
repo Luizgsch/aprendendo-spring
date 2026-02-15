c8de3adb23d49d9c48b7744b9046472530a0e20e
Luiz <luizgsch1@gmail.com>
2026-02-15 14:33:32 -0300

    desenvolvido api para aprendizado de spring security e spring data

---
+++ src/main/java/com/luiz/aprendendo_spring/controller/dtos/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package com.luiz.aprendendo_spring.controller.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+
+}
